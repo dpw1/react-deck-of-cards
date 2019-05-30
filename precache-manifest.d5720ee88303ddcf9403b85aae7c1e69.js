3f49295b3ac4f94951b2c33b17f4630cfb0a3180
dpw1 <diego.boarutto.fortes@gmail.com>
2019-05-30 19:23:35 +0100

    Updates

---
+++ precache-manifest.d5720ee88303ddcf9403b85aae7c1e69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb0f899af13f65a7046111418820e11",
+    "url": "/react-deck-of-cards/index.html"
+  },
+  {
+    "revision": "38599d8a8e5c918b54d2",
+    "url": "/react-deck-of-cards/static/css/main.385d7ddd.chunk.css"
+  },
+  {
+    "revision": "c1fadefe46b101b0e52c",
+    "url": "/react-deck-of-cards/static/js/2.82049b35.chunk.js"
+  },
+  {
+    "revision": "38599d8a8e5c918b54d2",
+    "url": "/react-deck-of-cards/static/js/main.923c2589.chunk.js"
+  },
+  {
+    "revision": "259725962be468c04416",
+    "url": "/react-deck-of-cards/static/js/runtime~main.9616842a.js"
+  }
+]);
